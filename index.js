774e9f1cf6ebeb98bf654dcde09e86e5d539d7aa
Pablo N Manzur <pablonicolasmanzur@gmail.com>
2024-09-26 17:18:11 -0300

    practica Post/Delete

--- index.js
+++ index.js
@@ -25,14 +25,14 @@ const materiasCursadas = [
     }
 
 ]
-
+//GET SIN PARAMETROS
 app.get("/materiasCursadas", (req, res)=> {
     res.send(materiasCursadas)
     console.log(materiasCursadas)
 })
-
-app.get("/materiasCursadas/:codIdentificacion", (req,res) =>{
-    const { codIdentificacion: codID } = req.params
+//GET CON PARAMETROS
+app.get("/materiasCursadas/:codID", (req,res) =>{
+    const { codID } = req.params
     const objetoConId = materiasCursadas.filter((el) => el.codIdentificacion == codID)
 
     if (!objetoConId.length > 0) {
@@ -44,6 +44,34 @@ app.get("/materiasCursadas/:codIdentificacion", (req,res) =>{
     }
 })
 
+//POST
+app.post("/materiasCursadas", (request, response) => {
+    const nuevaMateria = request.body
+    const matBuscada = materiasCursadas.find( m => m.codIdentificacion == nuevaMateria.codIdentificacion)
+    if (matBuscada == null) {
+        console.log("Nueva Materia agregada:", nuevaMateria)
+        materiasCursadas.push(nuevaMateria)
+        response.send(nuevaMateria)
+    }
+    else {
+       response.send({code:505, message: "Ya hay un elemento con este id"})
+    }
+})
+
+//DELETE
+app.delete("/materiasCursadas/:codId", (req, res)=> {
+    const { codId } = req.params
+    const indexMateria = materiasCursadas.findIndex((mat) => mat.codIdentificacion == codId)
+    
+    if (!(indexMateria == -1)){
+        materiasCursadas.splice(indexMateria, 1)
+        res.send({message:"Materia eliminada exitosamente", id: codId})
+        console.log("\nMaterias restantes",materiasCursadas)
+    } 
+ 
+    else res.send({code: 404, mensaje: "No exista elemento con ese id"})
+    
+})
 
 app.listen(PORT, () => {
     console.log(`Servidor corriendo en puerto ${PORT}`)
